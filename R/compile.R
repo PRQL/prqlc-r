402cd5af58452b2b51b5379fbf5005956a72d1b8
eitsupi <ts1s1andn@gmail.com>
2023-01-17 13:25:06 +0000

    more docs

--- R/compile.R
+++ R/compile.R
@@ -1,6 +1,22 @@
 #' @inherit compile
+#' @param prql_qyery,prql a PRQL query string.
 #' @param dialect a SQL dialect name to use. If not specified, the dialect contained in the query will be used.
+#' @details [prql_to_sql] is deprecated in favor of [prql_compile].
 #' @seealso available_dialects
+#' @examples
+#' "from mtcars | filter cyl > 6 | select [cyl, mpg]" |>
+#'   prql_compile()
+#'
+#' "from mtcars | filter cyl > 6 | select [cyl, mpg]" |>
+#'   prql_compile(format = FALSE, signature_comment = FALSE)
+#'
+#' "
+#' from mtcars
+#' filter cyl > 6
+#' select ![cyl]
+#' " |>
+#'   prql_compile("duckdb") |>
+#'   cat()
 #' @export
 prql_compile <- function(prql_query, dialect = NA, format = TRUE, signature_comment = TRUE) {
   if (length(dialect) == 1 && !(dialect %in% c(NA, dialects))) {
@@ -29,6 +45,7 @@ available_dialects <- function() {
   dialects
 }
 
+#' @rdname compile
 #' @export
 prql_to_sql <- function(prql) {
   .Deprecated("prql_compile")
