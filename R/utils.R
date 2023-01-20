68e3694a735612f0f0d894a859d696cf7f806503
sorhawell <sorhawell@gmail.com>
2023-01-21 00:08:04 +0100

    fix test errors

--- R/utils.R
+++ R/utils.R
@@ -2,13 +2,18 @@
 #'
 #' @param result a list where either element ok or err is NULL, or both if ok is a literal NULL.
 #' @param call context of error or string used for error printing, could be omitted.
-#'
 #' @return the ok-element of list , or a error will be thrown
 #'
 #' @examples
+#' #export unwrap for this example, only for internal use
+#' unwrap <- prqlr:::unwrap
+#'
+#' #unwrap a result with the ok-value "foo"
 #' unwrap(list(ok="foo",err=NULL))
+#'
+#' #unwrap an err-result
 #' tryCatch(
-#'   unwrap(ok=NULL, err = "something happen on the rust side"),
+#'   unwrap(ok=NULL, err = "something happend on the rust side"),
 #'   error = function(e) as.character(e)
 #' )
 unwrap = function(result, call=sys.call(1L)) {
@@ -30,15 +35,20 @@ unwrap = function(result, call=sys.call(1L)) {
 
   #if result is error, raise R errors. Modify the error formatting as necessary.
   if(is.null(result$ok) && !is.null(result$err)) {
-    stop(
-      paste(
-        result$err,
-        paste(
-          "\nwhen calling:\n",
-          paste(
-            capture.output(print(call)),collapse="\n")
-        )
-      ))
+
+    stop(result$err)
+
+    #add some custom error context, or just delete
+    # stop(
+    #   paste(
+    #     result$err,
+    #     paste(
+    #       "\nwhen calling:\n",
+    #       paste(
+    #         capture.output(print(call)),collapse="\n")
+    #     )
+    #   )
+    # )
   }
 
   #if not ok XOR error, then it must be another internal error.
