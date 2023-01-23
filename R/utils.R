07b64aed0d5169b1f82188a1812c47f8c3328167
eitsupi <ts1s1andn@gmail.com>
2023-01-23 13:58:02 +0000

    minor formatting and do not export the unwrap function

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,7 @@
-#' rust-like unwrapping of result. Useful to do error handling on the R side.
-#'
+#' @title Rust-like unwrapping of result. Useful to do error handling on the R side.
 #' @param result a list where either element ok or err is NULL, or both if ok is a literal NULL.
 #' @param call context of error or string used for error printing, could be omitted.
 #' @return the ok-element of list , or a error will be thrown
-#'
 #' @examples
 #' # export unwrap for this example, only for internal use
 #' unwrap <- prqlr:::unwrap
@@ -16,14 +14,10 @@
 #'   unwrap(ok = NULL, err = "something happend on the rust side"),
 #'   error = function(e) as.character(e)
 #' )
+#' @noRd
 unwrap <- function(result, call = sys.call(1L)) {
   # if not result
-  if (
-    !inherits(result, "Result") && (
-      !is.list(result) ||
-        !all(names(result) %in% c("ok", "err"))
-    )
-  ) {
+  if (!inherits(result, "Result") && (!is.list(result) || !all(names(result) %in% c("ok", "err")))) {
     stop("Internal error: cannot unwrap non result")
   }
 
