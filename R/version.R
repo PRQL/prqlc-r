f652d415019e40c5bc8ae2da19ed6ec2fcf5c461
eitsupi <ts1s1andn@gmail.com>
2023-01-23 10:47:02 +0000

    prql_compiler_version -> prql_version

--- R/version.R
+++ R/version.R
@@ -1,8 +1,8 @@
 #' @title prql-compiler's version
-#' @return a [numeric_version] with the version of the prql-compiler.
+#' @return a [numeric_version] with the version of the built-in prql-compiler.
 #' @examples
-#' prql_compiler_version()
+#' prql_version()
 #' @export
-prql_compiler_version <- function() {
+prql_version <- function() {
   numeric_version(compiler_version())
 }
