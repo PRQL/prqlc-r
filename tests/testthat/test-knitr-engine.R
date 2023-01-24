c255a9a3d65d87087adf5abac143ff69a5fee31a
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-01-24 15:33:16 +0000

    update tests

--- tests/testthat/test-knitr-engine.R
+++ tests/testthat/test-knitr-engine.R
@@ -1,8 +1,12 @@
+test_that("Set prql knitr engine", {
+  expect_true("prql" %in% names(knitr::knit_engines$get()))
+})
+
 test_that("Snapshot test of knitr-engine", {
   input <- file.path("files", "test-engine.Rmd")
   output <- tempfile(fileext = "md")
 
-  knitr::knit(input, output)
+  knitr::knit(input, output, quiet = TRUE)
 
   expect_snapshot(
     readLines(output) |>
