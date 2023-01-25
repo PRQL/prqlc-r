5dc66b16a6b520161355f61d65b51f2c9ab04bd4
eitsupi <ts1s1andn@gmail.com>
2023-01-25 16:51:37 +0000

    prints a SQL code block if there is no connection

--- R/knitr-engine.R
+++ R/knitr-engine.R
@@ -1,4 +1,8 @@
 #' @title PRQL knitr engine
+#' @description
+#' If options$connection is NULL, the output is SQL query.
+#' If options$connection is not NULL, the knitr's SQL engine is executed and the results are returned.
+#' @return A character string.
 #' @noRd
 eng_prql <- function(options) {
   prql_code <- options$code |>
@@ -6,10 +10,22 @@ eng_prql <- function(options) {
   sql_code <- prql_code |>
     prql_compile()
 
-  options$code <- sql_code
-
   # elm coincidentally provides the best syntax highlight for prql.
   options$engine <- "elm"
+
+  # Prints a SQL code block if there is no connection
+  if (is.null(options$connection)) {
+    options$comment <- ""
+    options$results <- "asis"
+    sql_code <- paste0(
+      "```sql\n",
+      sql_code,
+      "```\n"
+    )
+    return(knitr::engine_output(options, prql_code, sql_code))
+  }
+
+  options$code <- sql_code
 
   eng_sql <- utils::getFromNamespace("eng_sql", "knitr")
   eng_sql(options) |>
