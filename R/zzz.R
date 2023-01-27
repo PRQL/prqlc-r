d1bd748345418dc45a75de020e79006f6adfae66
eitsupi <ts1s1andn@gmail.com>
2023-01-27 13:37:51 +0000

    refactor: more appropriate onLoad event

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,13 @@
 .onLoad <- function(...) {
-  if (requireNamespace("knitr", quietly = TRUE)) {
+  if (isNamespaceLoaded("knitr") && "knit_engines" %in% getNamespaceExports("knitr")) {
     knitr::knit_engines$set(prql = eng_prql)
+  } else {
+    setHook(
+      packageEvent("knitr", "onLoad"), function(...) {
+        knitr::knit_engines$set(prql = eng_prql)
+      }
+    )
   }
+
+  invisible()
 }
