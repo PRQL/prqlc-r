684448f7aee4affb0c64fda2a4ebfc53eec1658a
eitsupi <ts1s1andn@gmail.com>
2023-02-05 03:13:14 +0000

    switch to use logo only image

--- dev/generate-sticker.R
+++ dev/generate-sticker.R
@@ -8,15 +8,15 @@
 logo_path <- file.path("man", "figures", "logo.png")
 
 logo_original <- magick::image_read(
-  "https://github.com/PRQL/prql-brand/raw/8ff2c16de4488535e1ef3cc2c51d675841032fac/logo/PNG/Logo%202.png"
+  "https://raw.githubusercontent.com/PRQL/prql-brand/c73248897ec3aadea97122f119280bb87b14934d/logo/PNG/logo-image.png"
 )
 
 sysfonts::font_add_google("Prompt")
 
-magick::image_crop(logo_original, "650x650+215+190") |>
+logo_original |>
   hexSticker::sticker(
     package = "prqlr", p_family = "Prompt", p_size = 50, p_y = 1.5,
-    s_x = 1, s_y = 0.75, s_width = 1, s_height = 1,
+    s_x = 1, s_y = 0.75, s_width = 1.1, s_height = 1.1,
     h_fill = "#010b37", h_color = "#595b82",
     dpi = 500,
     filename = logo_path
