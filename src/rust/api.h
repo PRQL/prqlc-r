6ad8b5c8b473f834350b3501a3db30a2e875c7af
eitsupi <ts1s1andn@gmail.com>
2024-05-10 12:58:28 +0000

    chore: bump savvy to 0.6.3

--- src/rust/api.h
+++ src/rust/api.h
@@ -1,6 +1,6 @@
-SEXP compile(SEXP prql_query, SEXP target, SEXP format, SEXP signature_comment);
-SEXP prql_to_pl(SEXP prql_query);
-SEXP pl_to_rq(SEXP pl_json);
-SEXP rq_to_sql(SEXP rq_json);
-SEXP compiler_version(void);
-SEXP prql_get_targets(void);
+SEXP savvy_compile__ffi(SEXP prql_query, SEXP target, SEXP format, SEXP signature_comment);
+SEXP savvy_prql_to_pl__ffi(SEXP prql_query);
+SEXP savvy_pl_to_rq__ffi(SEXP pl_json);
+SEXP savvy_rq_to_sql__ffi(SEXP rq_json);
+SEXP savvy_compiler_version__ffi(void);
+SEXP savvy_prql_get_targets__ffi(void);
