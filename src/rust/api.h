bf028f0955cd77320031105f6aa48ed30cf9df29
eitsupi <50911393+eitsupi@users.noreply.github.com>
2025-03-20 02:22:38 +0900

    chore: update auto generated files (#348)

--- src/rust/api.h
+++ src/rust/api.h
@@ -4,3 +4,4 @@ SEXP savvy_pl_to_rq__ffi(SEXP c_arg__pl_json);
 SEXP savvy_prql_get_targets__ffi(void);
 SEXP savvy_prql_to_pl__ffi(SEXP c_arg__prql_query);
 SEXP savvy_rq_to_sql__ffi(SEXP c_arg__rq_json);
+
